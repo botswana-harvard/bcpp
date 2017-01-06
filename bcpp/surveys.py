2a640d02ac72251a0aedb11bc77c9793659b9ac0
erikvw <ew2789@gmail.com>
2017-01-06 22:02:23 +0200

    change surveys declarations to use datetimes

--- bcpp/surveys.py
+++ bcpp/surveys.py
@@ -12,29 +12,29 @@ from survey.survey_schedule import SurveySchedule
 bcpp_year_one = SurveySchedule(
     name='bcpp-year-1',
     group_name='bcpp-survey',
-    start_date=(get_utcnow() - relativedelta(years=3)).date(),
-    end_date=(get_utcnow() - relativedelta(years=2)).date())
+    start=(get_utcnow() - relativedelta(years=3)),
+    end=(get_utcnow() - relativedelta(years=2)))
 
 bcpp_year_two = SurveySchedule(
     name='bcpp-year-2',
     group_name='bcpp-survey',
-    start_date=(get_utcnow() - relativedelta(years=2)).date(),
-    end_date=(get_utcnow() - relativedelta(years=1)).date())
+    start=(get_utcnow() - relativedelta(years=2)),
+    end=(get_utcnow() - relativedelta(years=1)))
 
 bcpp_year_three = SurveySchedule(
     name='bcpp-year-3',
     group_name='bcpp-survey',
-    start_date=(get_utcnow() - relativedelta(years=1)).date(),
-    end_date=get_utcnow().date())
+    start=(get_utcnow() - relativedelta(years=1)),
+    end=get_utcnow())
 
 # year 1 surveys
 bhs_survey_y1 = Survey(
     name='bhs',
     position=0,
     map_area='test_community',
-    start_date=(get_utcnow() - relativedelta(years=3)).date(),
-    end_date=(get_utcnow() - relativedelta(years=2)).date(),
-    full_enrollment_date=(get_utcnow() - relativedelta(years=2)).date()
+    start=(get_utcnow() - relativedelta(years=3)),
+    end=(get_utcnow() - relativedelta(years=2)),
+    full_enrollment_datetime=(get_utcnow() - relativedelta(years=2))
 )
 
 bcpp_year_one.add_survey(bhs_survey_y1)
@@ -44,18 +44,18 @@ bhs_survey_y2 = Survey(
     name='bhs',
     position=0,
     map_area='test_community',
-    start_date=(get_utcnow() - relativedelta(years=2)).date(),
-    end_date=(get_utcnow() - relativedelta(years=1)).date(),
-    full_enrollment_date=(get_utcnow() - relativedelta(years=1)).date()
+    start=(get_utcnow() - relativedelta(years=2)),
+    end=(get_utcnow() - relativedelta(years=1)),
+    full_enrollment_datetime=(get_utcnow() - relativedelta(years=1))
 )
 
 ahs_survey_y2 = Survey(
     name='ahs',
     position=1,
     map_area='test_community',
-    start_date=(get_utcnow() - relativedelta(years=2)).date(),
-    end_date=(get_utcnow() - relativedelta(years=1)).date(),
-    full_enrollment_date=(get_utcnow() - relativedelta(years=1)).date()
+    start=(get_utcnow() - relativedelta(years=2)),
+    end=(get_utcnow() - relativedelta(years=1)),
+    full_enrollment_datetime=(get_utcnow() - relativedelta(years=1))
 )
 bcpp_year_two.add_survey(bhs_survey_y2, ahs_survey_y2)
 
@@ -64,17 +64,17 @@ ahs_survey_y3 = Survey(
     name='ahs',
     position=0,
     map_area='test_community',
-    start_date=(get_utcnow() - relativedelta(years=1)).date(),
-    end_date=(get_utcnow()).date(),
-    full_enrollment_date=(get_utcnow()).date()
+    start=(get_utcnow() - relativedelta(years=1)),
+    end=(get_utcnow()),
+    full_enrollment_datetime=(get_utcnow())
 )
 ess_survey_y3 = Survey(
     name='ess',
     position=1,
     map_area='test_community',
-    start_date=(get_utcnow() - relativedelta(years=1)).date(),
-    end_date=(get_utcnow()).date(),
-    full_enrollment_date=(get_utcnow()).date()
+    start=(get_utcnow() - relativedelta(years=1)),
+    end=(get_utcnow()),
+    full_enrollment_datetime=(get_utcnow())
 )
 bcpp_year_three.add_survey(ahs_survey_y3, ess_survey_y3)
 
