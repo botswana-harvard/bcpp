e3e5fc738f8f8d4fc86d663adf6b7fdf9a406a8a
erikvw <ew2789@gmail.com>
2017-01-18 06:06:28 +0200

    use app config view mixin

--- bcpp/views/administration_view.py
+++ bcpp/views/administration_view.py
@@ -4,9 +4,10 @@ from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 from edc_base.view_mixins import EdcBaseViewMixin
+from edc_dashboard.view_mixins import AppConfigViewMixin
 
 
-class AdministrationView(EdcBaseViewMixin, TemplateView):
+class AdministrationView(EdcBaseViewMixin, AppConfigViewMixin, TemplateView):
 
     template_name = 'bcpp/administration.html'
 
@@ -16,5 +17,5 @@ class AdministrationView(EdcBaseViewMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update(navbar_selected='home')
+        context.update(navbar_selected='administration')
         return context
