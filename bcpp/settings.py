fb114a8b5185a698353dd0f87fd593aeba1b3658
Erik van Widenfelt <ew2789@gmail.com>
2017-02-09 19:28:55 +0200

    Update settings.py

--- bcpp/settings.py
+++ bcpp/settings.py
@@ -135,7 +135,7 @@ WSGI_APPLICATION = 'bcpp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'edc1',
+        'NAME': 'edc',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
