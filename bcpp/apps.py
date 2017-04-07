bfdb06fb0a48aebdfc3c1d8c09207ff7cc8c408c
KeletsoBotsalano <kbotsalano@bhp.org.bw>
2017-04-07 17:33:27 +0200

    config path

--- bcpp/apps.py
+++ bcpp/apps.py
@@ -47,7 +47,9 @@ from .navbars import navbars
 style = color_style()
 ANONYMOUS_CONSENT_GROUP = 'anonymous'
 config = configparser.RawConfigParser()
-config.read(os.path.join(settings.CONFIG_PATH))
+config.read(os.path.join(settings.ETC_DIR,
+                         settings.APP_NAME,
+                         settings.CONFIG_FILE))
 
 
 class AppConfig(DjangoAppConfig):
