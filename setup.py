e8d997af1f28173e2772dc671d7ebfb26925c1e6
erikvw <ew2789@gmail.com>
2017-04-09 00:00:47 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='bcpp',
-    version='0.1.17',
+    version='0.1.18',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
