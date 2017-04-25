31fbc4ca501be92c390e0b25680c7d3511857bf2
erikvw <ew2789@gmail.com>
2017-04-25 12:29:19 +0200

    fabfile

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,3 +1,4 @@
 from .deploy import deploy_centralserver, deploy_client, deploy_nodeserver
 from .utils import restore_media_folder
 from .update import query_tx_task
+from edc_fabric import fabfile as common
