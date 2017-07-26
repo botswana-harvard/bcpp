a0228d13ca798474ebd2035cd812f5601d84d78c
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-07-26 16:26:18 +0200

    update app_names for navbars

--- bcpp/navbars.py
+++ bcpp/navbars.py
@@ -4,8 +4,8 @@ from edc_lab_dashboard.navbars import navbar_items as edc_lab_navbar_items
 navbars = {}
 navbar_items = []
 config = [
-    ('plot', 'plots', 'fa-building', 'listboard_url_name'),
-    ('household', 'households', 'fa-home', 'listboard_url_name'),
+    ('plot_dashboard', 'plots', 'fa-building', 'listboard_url_name'),
+    ('household_dashbaord', 'households', 'fa-home', 'listboard_url_name'),
     ('enumeration', 'enumeration', 'fa-sitemap', 'listboard_url_name'),
     ('member_dashboard', 'members', 'fa-users', 'listboard_url_name'),
     ('bcpp_subject_dashboard', 'subjects',
