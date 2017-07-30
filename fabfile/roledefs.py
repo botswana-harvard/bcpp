8632060809d912f0407fff479a318e3528889e22
Gofaone Magodi <magodign@magodign.local>
2017-07-30 13:54:06 +0200

    define sefophe,lerala roledefs

--- fabfile/roledefs.py
+++ fabfile/roledefs.py
@@ -66,6 +66,19 @@ def get_maunatlala():
     return hosts
 
 
+def get_lerala():
+    hosts = ['bcpp0{}'.format(i + 10) for i in range(0, 15)]
+    hosts.append('bcpp057')
+    return hosts
+
+
+def get_sefophe():
+    hosts = ['bcpp0{}'.format(i + 40) for i in range(0, 15)]
+    hosts.append('bcpp026')
+    hosts.append('bcpp035')
+    return hosts
+
+
 roledefs = {
     'deployment_hosts': ['localhost'],
     'mmankgodi': get_mmankgodi(),
@@ -76,5 +89,7 @@ roledefs = {
     'bokaa': get_bokaa(),
     'maunatlala': get_maunatlala(),
     'ramokgonami': get_ramokgonami(),
+    'lerala': get_lerala(),
+    'sefophe': get_sefophe(),
     'testhosts': ['bcpp075', 'bcpp076', 'bcpp077', 'bcpp078', 'bcpp080', 'bcpp081'],
 }
