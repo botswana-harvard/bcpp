89c0900c10b1e28e70805db53cc78266d0f9cb89
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-09-02 16:52:23 +0200

    fix target_path

--- scripts/retry_copy_tx_files.sh
+++ scripts/retry_copy_tx_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /Users/django/.venvs/bcpp/bin/activate && \
 cd /Users/django/source/bcpp && \
-python manage.py export_transactions --send_only=True --target_path=/Users/django/media/transactions/incoming
+python manage.py export_transactions --send_only=True --target_path=/home/django/media/transactions/incoming
