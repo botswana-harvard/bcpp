4e719e048f8507329d2d835b2f6307a7d43ecb87
Itumeleng Mosweu <imosweuk@gmail.com>
2017-09-07 11:55:27 +0200

    fix tag in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='bcpp',
-    version='0.1.66',
+    version='0.1.65',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
