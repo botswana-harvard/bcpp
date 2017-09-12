b6acdf168da2efae0851ed8aea9fb2ef6539fe0e
erikvw <ew2789@gmail.com>
2017-09-12 14:55:55 +0200

    add custom json parser

--- bcpp/apps.py
+++ bcpp/apps.py
@@ -4,6 +4,7 @@ import os
 
 from bcpp_follow.apps import AppConfig as BaseBcppFollowAppConfig
 from bcpp_subject.apps import AppConfig as BaseBcppSubjectAppConfig
+from bcpp_subject.parsers import schedule_appt_date_parser
 from bcpp_subject_dashboard.apps import AppConfig as BaseBcppSubjectDashboardAppConfig
 from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU
 from dateutil.tz import gettz
@@ -250,6 +251,7 @@ class EdcSyncAppConfig(BaseEdcSyncAppConfig):
     edc_sync_files_using = True
     server_ip = config['edc_sync'].get('server_ip')
     base_template_name = 'bcpp/base.html'
+    custom_json_parser = [schedule_appt_date_parser]
 
 
 class EdcSyncFilesAppConfig(BaseEdcSyncFilesAppConfig):
