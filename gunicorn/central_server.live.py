84874ad0737db328f4602053bdb77d1cd47b3f70
Coulson Kgathi <ckgathi@gmail.com>
2018-08-09 09:45:33 +0200

    gunicorn conf

---
+++ gunicorn/central_server.live.py
@@ -0,0 +1,19 @@
+# uat.ambition gunicorn.conf
+import os
+
+from pathlib import Path
+
+SOURCE_ROOT = str(Path(os.path.dirname(os.path.abspath(__file__))).parent)
+
+errorlog = os.path.join(
+    SOURCE_ROOT, 'log/bcpp-live-central-server-gunicorn-error.log')
+accesslog = os.path.join(
+    SOURCE_ROOT, 'log/bcpp-live-central-server-gunicorn-access.log')
+loglevel = 'debug'
+pidfile = os.path.join(SOURCE_ROOT, 'run/bcpp-live-central-server.pid')
+
+workers = 2  # the number of recommended workers is '2 * number of CPUs + 1'
+
+raw_env = ['DJANGO_SETTINGS_MODULE=bcpp.settings']
+
+bind = "127.0.0.1:9000"
